f17d6c97d02da6ddab12b2ab7cfbe4498a7f9ec1
Daniel Hernandez <danielhernandez918@gmail.com>
2022-04-05 23:45:32 -0700

    updated

--- BankAccount.java
+++ BankAccount.java
@@ -2,17 +2,23 @@ import java.util.Scanner;
 public class BankAccount {
     private double checkingBalance;
     private double savingsBalance;
-    private static int numberOfAccounts =0;
+    private static int numberOfAccounts = 0;
+    private static double totalMoney = 0;
     public BankAccount(){}
     
     public BankAccount(double checking, double savings) {
         checkingBalance  = checking;
         savingsBalance = savings;
         numberOfAccounts++;
+        totalMoney += checkingBalance + savingsBalance;
     }
 
     public static int accountCount() {
         return numberOfAccounts;
+    }
+
+    public static double allMoney() {
+        return totalMoney;
     }
 
     public double getChecking() {
@@ -25,28 +31,45 @@ public class BankAccount {
     public void depositChecking() {
         Scanner sc= new Scanner(System.in); 
         System.out.println("Please enter amount deposited:");
-        this.checkingBalance += sc.nextInt();
+        double amount = sc.nextInt();
+        this.checkingBalance += amount;
+        totalMoney+= amount;
     }
     public void depositSavings() {
         Scanner sc= new Scanner(System.in); 
         System.out.println("Please enter amount deposited:");
-        this.savingsBalance += sc.nextInt();
+        double amount = sc.nextInt();
+        this.savingsBalance += amount;
+        totalMoney+= amount;
     }
     public void withdrawChecking() {
-        Scanner sc= new Scanner(System.in); 
-        System.out.println("Please enter amount deposited:");
-        this.checkingBalance -= sc.nextInt();
-    }
+        if (this.checkingBalance > 0) {
+            Scanner sc= new Scanner(System.in); 
+            System.out.println("Please enter amount to withdraw:");
+            double amount = sc.nextInt();
+            this.checkingBalance -= amount;
+            totalMoney-= amount;
+            }
+            else {
+                System.out.println("Insufficient Funds");
+            }
+        }
     public void withdrawSavings() {
+        if (this.savingsBalance > 0) {
         Scanner sc= new Scanner(System.in); 
-        System.out.println("Please enter amount deposited:");
-        this.savingsBalance -= sc.nextInt();
+        System.out.println("Please enter amount to withdraw:");
+        double amount = sc.nextInt();
+        this.savingsBalance -= amount;
+        totalMoney-= amount;
+        }
+        else {
+            System.out.println("Insufficient Funds");
+        }
     }
     public void displayAccounts() {
         double checkingAmt = this.getChecking();
         double savingsAmt = this.getSavings();
         System.out.printf("Checking Balance: %s, Savings Balance: %s \n\n", checkingAmt, savingsAmt);
-        
     }
 
 
--- BankAccountTest.java
+++ BankAccountTest.java
@@ -1,12 +1,14 @@
 public class BankAccountTest {
     
     public static void main(String[] args) {
-        BankAccount user1 = new BankAccount(100, 200);
+        BankAccount user1 = new BankAccount(0, 200);
+        System.out.println(BankAccount.allMoney());
         System.out.println(BankAccount.accountCount());
         user1.depositChecking();
         user1.withdrawChecking();
         user1.depositSavings();
-        System.out.println(user1.getChecking());
+        // System.out.println(user1.getChecking());
         user1.displayAccounts();
+        System.out.println(BankAccount.allMoney());
     }
 }
